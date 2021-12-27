2be2bcb7f7bf31294da45b3bd0364bf62fffe4b8
Stuart Tottle <stuarttottle@hotmail.com>
2021-12-27 18:28:58 +0000

    updated react

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import './App.scss';
-import { Bem } from './shared/bem';
-import Content from './shared/Content';
+import logo from './logo.svg';
+import './App.css';
 
-const bem = new Bem('container');
-
-const App: React.FC = () => (
-  <div className={bem.b()}>
-    <div className={bem.e('header')}>
-      <h1>Stuart Tottle</h1>
-      <h2>Software Engineer</h2>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    <div className={bem.e('content')}>
-      <Content />
-    </div>
-  </div>
-);
+  );
+}
 
 export default App;
